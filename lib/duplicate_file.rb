74250a6f036e8c51d9b4817fc4b8d8913064e721
TakuKobayshi <>
2017-08-23 17:32:59 +0900

    grouping and unique! to duplicated file

--- lib/duplicate_file.rb
+++ lib/duplicate_file.rb
@@ -1,8 +1,22 @@
 require "duplicate_file/version"
 module DuplicateFile
-  def self.search(root_path)
+  def self.group_by_checksum(root_path)
+    all_files = Dir.glob(root_path + "**/*").select{|path| File.file?(path) }
+    all_files.group_by{|path| Digest::MD5.file(path).to_s }
   end
 
   def self.unique!(root_path)
+    stay_pathes = []
+    deleted_pathes = []
+    checksum_filepath_group = group_by_checksum(root_path)
+    checksum_filepath_group.each do |checksum, filepathes|
+      dupricate_pathes = filepathes[1..(filepathes.size - 1)] || []
+      stay_pathes << filepathes.first
+      dupricate_pathes.each do |path|
+        deleted_pathes << path
+        File.delete(path)
+      end
+    end
+    return stay_pathes.uniq, deleted_pathes
   end
 end
